af7064dade2db701418db06b5bc8c94c6569f799
VikaEra <vikastav26@yandex.ru>
2024-05-18 10:47:25 +0300

    Main.java added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int startScore = 100; // на счету у клиента
+        int replenishment = 1200; // пополнение счета
+        int totalAmount; // итоговый счет
+        int bonus; // количество бонусных рублей
+        boolean refill = (replenishment > 1000); // пополнение счета более, чем на 1000 рублей (принимается за true)
+
+        if (refill) {
+            bonus = replenishment / 100; // бонусные рубли
+            totalAmount = startScore + replenishment + bonus; // итоговый счет
+        } else {
+            bonus = 0; // бонусные рубли = 0, бонусов нет
+            totalAmount = startScore + replenishment; // итоговый счет
+        }
+
+        System.out.println("Итоговый счет: " + totalAmount);
+        System.out.println("Бонусные рубли: " + bonus);
+
     }
 }
+
